26671ed1b9be03820a7da6e608bc8389b27790a7
Evgenii Terechkov <evg@altlinux.org>
2019-08-18 17:31:31 +0700

    Initial type autoguess realization

--- zabbix-import.py
+++ zabbix-import.py
@@ -7,6 +7,7 @@ import sys
 
 import urllib3
 urllib3.disable_warnings()
+import yaml
 
 from pyzabbix import ZabbixAPI
 from pprint import pprint
@@ -39,23 +40,69 @@ def get_zabbix_connection(zbx_url, zbx_user, zbx_password):
 
     raise Exception("Some error in pyzabbix or py_zabbix module, see logs")
 
+def guess_yaml_type(yml, xml_exported=False):
+    "Return string of guessed YAML file type (group, host, ...)"
+    if xml_exported:
+        if 'groups' in yml and not 'templates' in yml and not 'hosts' in yml: return 'group'
+        if 'templates' in yml: return 'template'
+        if 'value_maps' in yml: return 'valuemap'
+        if 'maps' in yml: return 'map'
+        if 'screens' in yml: return 'screen'
+        if 'hosts' in yml: return 'host'
+    else:
+        if yml.keys() >= {"algorithm", "goodsla"}: return 'service'
+        if yml.keys() >= {"proxy_hostid"}: return 'proxy'
+        if yml.keys() >= {"maintenance_type"}: return 'maintenance'
+        if yml.keys() >= {"alias"}: return 'user'
+        if yml.keys() >= {"exec_path"}: return 'mediatype'
+        if yml.keys() >= {"users_status"}: return 'usergroup'
+        if yml.keys() >= {"operations"}: return 'action'
+        if yml.keys() >= {"hostid", "macro"}: return 'usermacro'
+        if yml.keys() >= {"macro"}: return 'globalmacro'
+        if yml.keys() >= {"imagetype"}: return 'image'
+        
+    return 'autoguess'
+
 def main(zabbix_, yaml_file, file_type):
     api_version = zabbix_.apiinfo.version()
     logging.info('Destination Zabbix server version: {}'.format(api_version))
 
+    with open(yaml_file, 'r') as f:
+        yml = yaml.safe_load(f)
+    logging.debug('Got following YAML: {}'.format(yml))
+
+    xml_exported = False
+    
+    if 'zabbix_export' in yml:
+        logging.info('Loading from XML-exported YAML')
+        xml_exported = True
+        if 'version' in yml['zabbix_export']:
+            logging.info('Source Zabbix server version: {}'.format(yml['zabbix_export']['version']))
+    else:
+        logging.info('Loading from JSON-exported/raw YAML')
+    if file_type == 'autoguess':
+        if xml_exported: file_type = guess_yaml_type(yml['zabbix_export'], xml_exported=xml_exported)
+        else: file_type = guess_yaml_type(yml, xml_exported=xml_exported)
+        if file_type == 'autoguess':
+            logging.error("Cant guess object type, exiting...")
+            sys.exit(1)
+        logging.info('Guessed file type: {}'.format(file_type))
+
 def parse_args():
+    "Return parsed CLI args"
     parser = argparse.ArgumentParser(description="Import Zabbix object from YAML dump")
     parser.add_argument ("--debug", action="store_true", help="Show debug output")
     parser.add_argument("--zabbix-url", action="store", required=True)
     parser.add_argument("--zabbix-username", action="store", required=True)
     parser.add_argument("--zabbix-password", action="store", required=True)
-    parser.add_argument("--type", choices=["autoguess", "host", "group", "template", "valuemap", "screen", "map" ], default="autoguess", help="Zabbix object type, default is %(default)s")
+    parser.add_argument("--type", choices=["autoguess", "host", "group", "template", "valuemap", "screen", "map", "service", "maintenance", "user", "mediatype", "usergroup", "action", "usermacro", "proxy", "image", "globalmacro"], default="autoguess", help="Zabbix object type, default is %(default)s")
     parser.add_argument("FILE", help="YAML file to import from")
 
     args = parser.parse_args()
     return args
 
 def init_logging(level):
+    "Initialize logging"
     logger_format_string = '%(levelname)-8s %(message)s'
     logging.basicConfig(level=level, format=logger_format_string, stream=sys.stdout)
 
@@ -67,5 +114,5 @@ if __name__ == "__main__":
 
     zabbix_ = get_zabbix_connection(args.zabbix_url, args.zabbix_username, args.zabbix_password)
 
-    logging.info("Trying to load Zabbix {} from: {}".format(args.type, os.path.abspath(args.FILE)))
+    logging.info("Trying to load Zabbix object (type: {}) from: {}".format(args.type, os.path.abspath(args.FILE)))
     main(zabbix_=zabbix_, yaml_file=args.FILE, file_type=args.type)
