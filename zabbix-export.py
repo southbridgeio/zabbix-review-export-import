d4d97052713f371bc2fc4433c2f8541229a44429
Evgenii Terechkov <evg@altlinux.org>
2019-09-10 19:32:03 +0700

    Delete usergroup id from users definition

--- zabbix-export.py
+++ zabbix-export.py
@@ -215,7 +215,10 @@ def main(zabbix_, save_yaml, directory):
     logging.info("Processing users...")
     users = zabbix_.user.get(selectMedias='extend', selectMediatypes='extend', selectUsrgrps='extend')
     userid2user = {}            # key: userid, value: user alias
-    for u in users: userid2user[u['userid']] = u['alias']
+    for u in users:
+        userid2user[u['userid']] = u['alias']
+        for ug in u['usrgrps']:
+            del ug['usrgrpid']
     dumps_json(object='users', data=users, key='alias', save_yaml=save_yaml, directory=directory, drop_keys=["userid", "attempt_clock", "attempt_failed", "attempt_ip"])
 
     logging.info("Processing proxy...")
